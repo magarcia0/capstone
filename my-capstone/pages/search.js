cfeb4d3bebf3940f923af5ff7b3e5d3f0affa026
Marco <>
2022-03-20 21:38:50 -0700

    Fix search

--- my-capstone/pages/search.js
+++ my-capstone/pages/search.js
@@ -1,77 +1,38 @@
-import Head from "next/head";
-import Accordion from "../components/accordion";
-import Exercise from "../components/exercise";
-import { baseUrl, fetchApi } from "../utils/fetchApi";
-import Layout, { siteTitle } from "../components/layout";
-//import Link from 'next/link';
-//import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import { useEffect, useState } from 'react';
+import Layout from "../components/layout"
+import Products from "../components/products"
 
-//const defaultEndpoint='https://exercisedb.p.rapidapi.com/exercises/target/biceps'
+const defaultEndpoint = "http://localhost:3000/api/data";
 
-/*export async function getServerSideProps() {
-  const res = await fetch(defaultEndpoint);
-  const data = await res.json();
-  return{
-    props: {
-      data
-    }
-  }
-}*/
+const Frontend = () => {
+    const [allProducts, setAllProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [filters, setFilters] = useState({
+        s: ''
+    });
 
-export async function getStaticProps() {
-  //const res = await fetchApi(`${baseUrl}`);
-  //const exerciseBiceps = await res.json();
-  const exerciseBiceps = await fetchApi(`${baseUrl}`);
+    useEffect( () => {
+        (
+            async () => {
+                const response = await fetch(defaultEndpoint);
+                const content = await response.json();
+                setAllProducts(content);
+                setFilteredProducts(content);
+            }
+        )()
+    }, []);
 
-  return {
-    props: {
-      exerciseBiceps,
-    },
-  };
-}
+    useEffect( () => {
+        let products = allProducts.filter(p => p.target.toLowerCase().indexOf(filters.s.toLowerCase()) >= 0)
 
-export default function Search({ exerciseBiceps }) {
-  //  const { data: session, status } = useSession();
-  const { results = [] } = exerciseBiceps;
+        setFilteredProducts(products);
+    }, [filters]);
 
-  return (
-    <>
+    return (
     <Layout>
-        <title> Search - {siteTitle}</title>
-        <br />
-
-      <div>
-        <Accordion
-          title="Biceps"
-          content={
-            <div>
-            <table>
-              <thead>
-                <tr>
-                  <th>Name of Exercise</th>
-                </tr>
-              </thead>
-              {results.map(exercise => {
-                //<Exercise exercise={exercise} key={exercise.id} />
-                const { id, name, gifURL } = exercise;
-                return(
-                  <li key={id} className="card">
-                    <h3>{name}</h3>
-                    <img src={ gifURL }></img>
-                    
-                    </li>
-                )
-              })
-              }
-            </table>
-            </div>
-          }
-        />
-        <Accordion title="Trapezius" content="this is content 2" />
-        <Accordion title="Triceps" content="this is content 3" />
-        <Accordion title="Shoulders" content="this is content 4" />
-      </div>
+        <Products products={ filteredProducts } filters={ filters } setFilters={ setFilters } />
     </Layout>
-    </>
-  );
-}
+    );
+};
+
+export default Frontend;
--- my-capstone/utils/fetchApi.js
+++ my-capstone/utils/fetchApi.js
@@ -1,12 +1,10 @@
 import axios from "axios"
 //export const baseUrl = "https://exercisedb.p.rapidapi.com/exercises"
-export const baseUrl = "https://pizza-and-desserts.p.rapidapi.com/pizzas"
 
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-    //  "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-      "x-rapidapi-host": "pizza-and-desserts.p.rapidapi.com",
+      "x-rapidapi-host": "exercisedb.p.rapidapi.com",
       "x-rapidapi-key": process.env.RAPID_API_SECRET,
     },
   })
