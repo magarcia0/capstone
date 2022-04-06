ddcf8a6d58235754c8574d79519f4370f9a296cf
Marco <>
2022-04-05 22:53:32 -0700

    adds header in postToDatabase()

--- my-capstone/pages/newpost.js
+++ my-capstone/pages/newpost.js
@@ -34,7 +34,9 @@ const routeJournal= e => {
 const postToDatabase = async () => {
   const response = await fetch('api/form', {
     method: 'POST',
-
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body:JSON.stringify({
       title: title, 
       workout: workout,
