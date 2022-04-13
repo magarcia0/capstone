a2ed9baf1fd9ba3e6415bc6c63fb2004cf900362
Marco <>
2022-04-12 23:56:18 -0700

    fixes workout page banner

--- my-capstone/pages/workouts/index.js
+++ my-capstone/pages/workouts/index.js
@@ -16,7 +16,7 @@ export default function Workouts() {
       <div className="min-h-screen p-10">
         <div className="space-y-10 md:space-y-0 md:grid ">
           <div className=" dark:text-white md:flex md:flex-col md:justify-center"></div>
-          <div className="rounded-md bg-red-800 dark:bg-gradient-to-b dark:from-slate-700 dark:to-slate-800">
+          <div className="rounded-md  bg-gradient-to-b from-slate-700 to-slate-800">
             <h2 className="text-white text-2xl md:text-4xl lg:text-6xl font-bold mb-4">
               Workout Entries
             </h2>
