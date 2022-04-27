4c414d1379f960ae3e67fe1d758a8d55a10333f4
Marco <>
2022-04-27 11:46:46 -0700

    fix workout endpoint

--- my-capstone/pages/workouts/index.js
+++ my-capstone/pages/workouts/index.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { getSession } from "next-auth/react";
 
-const defaultEndpoint = "https://www.wildcat.plus/api/workoutid";
+const defaultEndpoint = "https://wildcat.plus/api/workoutid";
 
 export const getServerSideProps = async (context) => {
   const session = await getSession(context);
