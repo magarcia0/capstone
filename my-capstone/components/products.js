da33c12ee2cd608e9b698e0eff7f626c5d657be4
Marco <>
2022-05-04 19:15:44 -0700

    change endpoint to prod

--- my-capstone/components/products.js
+++ my-capstone/components/products.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
-//const defaultEndpoint = "https://wildcat.plus/api/workoutid";
-const defaultEndpoint = "http://localhost:3000/api/workoutid";
+const defaultEndpoint = "https://wildcat.plus/api/workoutid";
+//const defaultEndpoint = "http://localhost:3000/api/workoutid";
 
 const Products = (props) => {
   const [allWorkouts, setAllWorkouts] = useState();
